6a2742380172e2c47a2d5cf58f7c13ba012ba3dc
Soorena Soleimani <s.soorena@gmail.com>
2018-12-13 01:56:36 +0330

    Remove day-2

--- 7-testing/src/ContentToggle.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ContentToggle from "./ContentToggle";
-import { Simulate } from "react-dom/test-utils";
-
-it("renders closed initially");
-it("opens when clicked");
-it("closes when clicked after being open");
